9299ae564c198b3e0a44da25b42295e78fff3511
zhou-yg <zhou.y.g.890@gmail.com>
2023-04-08 18:24:02 +0800

    feat: initialize cpt

---
+++ packages/source-subscriber/views/todos.tsx
@@ -0,0 +1,13 @@
+import { useSignal } from '@polymita/connect/dist/react'
+import React, { useState } from 'react'
+
+const Todos = () => {
+
+  return (
+    <div>
+      todos
+    </div>
+  )
+}
+
+export default Todos
