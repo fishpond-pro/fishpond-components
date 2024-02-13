1bea0afd0ad455d28fb5968e60dff5feecddd413
zhou-yg <zhou.y.g.890@gmail.com>
2024-02-13 11:07:24 +0800

    feat: patch

---
+++ scripts/patch.js
@@ -0,0 +1,109 @@
+const { join, resolve, parse, relative } = require('path')
+const { spawn, exec } = require('child_process')
+const chalk = require('chalk')
+const { existsSync, mkdirSync, readFileSync, fstat, readdirSync, writeFileSync } = require('fs')
+
+const SHOULD_RELEASE = !!process.env.RELEASE
+
+console.log('SHOULD_RELEASE: ', SHOULD_RELEASE);
+
+const packagesPath = join(__dirname, '../packages/')
+const allPackages = readdirSync(packagesPath).filter(packagesPath => packagesPath !== 'polymita').map(pkg => {
+  return join(packagesPath, pkg)
+})
+
+const PKG = 'package.json'
+
+const [specificModule] = process.argv.slice(2)
+console.log('specificModule: ', specificModule);
+
+async function buildDeps () {
+}
+
+function build(cwd) {
+  if (specificModule && !(new RegExp(`${specificModule}$`).test(cwd))) {
+    console.log(`skip building ${cwd}`)
+    return Promise.resolve()
+  }
+
+  console.log(`start building ${chalk.green(cwd)} \n`)
+
+  return new Promise((resolve, reject) => {
+    const instance = spawn('npm', ['run', 'build'], {
+      cwd,
+      stdio: [process.stdin, process.stdout, 'pipe']
+    })
+    let hasError = false
+    instance.stderr.on('data', (d) => {
+      console.error(`${chalk.red('error')}:${d}`)
+      hasError = true
+    })
+
+    instance.on('close', (code, signal) => {
+      console.log('code, signal: ', code, signal);
+      if (code || hasError) {
+        console.log(chalk.red(`build ${cwd} failed`));
+        reject()
+      } else {
+        console.log(chalk.green(`build ${cwd} success`));
+        resolve()
+      }
+    })
+  })
+}
+
+function publish (package) {
+  return new Promise(resolve => {
+    console.log('npm publish');
+    exec(`pnpm publish`, { cwd: package }, (err, stdout) => {
+      if (err) {
+        throw err
+      }
+      if (stdout) {
+        console.log(stdout);
+      }
+      resolve()
+    })
+  })
+}
+
+function commit () {
+  return new Promise(resolve => {
+    const versions = allPackages.map(dir => {
+      const pkgJSON = JSON.parse(readFileSync(join(dir, PKG)).toString())
+      return `${pkgJSON.name}@${pkgJSON.version}`
+    }).join(' ');
+    exec(`git commit -a -m "release: ${versions} "`, (err, stdout) => {
+      if (err) {
+        throw err
+      }
+      if (stdout) {
+        console.log(stdout);
+      }
+      resolve()
+    })
+  })
+}
+
+function upgradePatch(dirPath) {
+  const pkgJSONPath = join(dirPath, 'package.json')
+  const pkgJSON = require(pkgJSONPath)
+  pkgJSON.version = pkgJSON.version.replace(/\d+$/, (w) => parseInt(w) + 1)
+  writeFileSync(pkgJSONPath, JSON.stringify(pkgJSON, null, 2))
+}
+
+console.time('release')
+
+buildDeps().then(() => {
+  return Promise.all(allPackages.map(dir => {
+  })).then(async () => {
+    Promise.all(allPackages.map(upgradePatch))
+      .then(() => {
+        return commit()
+      }).then(() => {
+        return Promise.all(allPackages.map(publish))
+      }).then(() => {
+      });
+  })
+  
+})
