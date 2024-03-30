1b99c66495a305a3b4dfee37dbef8bbe3ab6ccb4
zhou-yg <zhou.y.g.890@gmail.com>
2024-03-30 14:28:43 +0800

    feat(cs): clear

--- packages/channel-subscriber/app/polymita/signalsMap.ts
+++
@@ -1,9 +0,0 @@
- import channel from '../../signals/channel'
- import menusLogic from '../../signals/menusLogic'
- import rss from '../../signals/rss'
-
-export const signalMap = {
-  channel,
-  menusLogic,
-  rss,    
-}
