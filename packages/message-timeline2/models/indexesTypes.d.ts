152b01551092390dc2b689c83c91857fedfd0088
zhou-yg <zhou.y.g.890@gmail.com>
2024-04-01 10:58:27 +0800

    feat(mt): refactor files

--- packages/message-timeline2/models/indexesTypes.d.ts
+++ packages/message-timeline2/models/indexesTypes.d.ts
@@ -1,12 +1,5 @@
 export {
   ChannelRecord,
   Message,
-  MessageContent,
-  Polymita_channel_subscriber_SubscribedChannel,
-  Polymita_channel_subscriber_RSS,
-  Polymita_channel_subscriber_RPA,
-  Polymita_channel_subscriber_Directory,
-  Polymita_channel_subscriber_Polymita_message_timeline2_ChannelRecord,
-  Polymita_channel_subscriber_Polymita_message_timeline2_Message,
-  Polymita_channel_subscriber_Polymita_message_timeline2_MessageContent 
+  MessageContent 
 } from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/message-timeline2/models/customPrismaClient/client/index'
