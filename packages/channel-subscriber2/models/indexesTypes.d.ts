ce994e640f0ee779b303fe27a6bd5e815a3d84cd
zhou-yg <zhou.y.g.890@gmail.com>
2024-06-01 17:06:49 +0800

    feat: clear清理旧components

--- packages/channel-subscriber2/models/indexesTypes.d.ts
+++
@@ -1,6 +0,0 @@
-export {
-  SubscribedChannel,
-  RSS,
-  RPA,
-  Directory 
-} from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/channel-subscriber2/models/customPrismaClient/client/index'
