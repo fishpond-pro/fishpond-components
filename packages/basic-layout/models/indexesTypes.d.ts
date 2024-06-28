af64a1428ed8a4664ee42d0a566a4f545aff9267
zhou-yg <zhou.y.g.890@gmail.com>
2024-06-28 21:50:03 +0800

    feat(bl): clear model

--- packages/basic-layout/models/indexesTypes.d.ts
+++
@@ -1,5 +0,0 @@
-export {
-  ChannelRecord,
-  Message,
-  MessageContent 
-} from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/basic-layout/models/customPrismaClient/client/index'
