f124c1f86cc859f4a01a367fb7351aa38327511a
zhou-yg <zhou.y.g.890@gmail.com>
2024-07-14 23:26:25 +0800

    feat: clear

--- packages/rss-sources-add/app/main/page.tsx
+++ packages/rss-sources-add/app/main/page.tsx
@@ -3,8 +3,6 @@ import rssSignal from '@polymita/rss-sources/dist/signals/rss'
 import Sources from '@polymita/rss-sources/dist/views/RssSources'
 import menus from '@/models/rsshub-source-menu.json'
 import rsshubSourcesMock from '@/models/rsshub-sources.json'
-import { toRSS_JSON } from '@/shared/utils'
-import sourceMock2 from '@/shared/rss-mock'
 import '@/app/polymita/views/AddRSSSource'
 
 export default () => {
@@ -13,6 +11,7 @@ export default () => {
     subscribed: [],
     menus,
     onQueryRssSources: async (arg) => {
+      console.log('[onQueryRssSources] arg: ', arg);
       const r = arg.map(([g, subGroup]) => {
         return rsshubSourcesMock.filter(item => 
           item.group === g && item.subGroup === subGroup
@@ -22,16 +21,6 @@ export default () => {
         tables: typeof item.tables === 'string' ? [item.tables] : item.tables
       }));
       return r
-    },
-    onQueryPreviews: async (form) => {
-      console.log('[onQuery] form: ', form);
-      return toRSS_JSON(sourceMock2).item
-    },
-    onSubmit: (...args) => {
-      console.log('[onSubmit] form: ', args);
-    },
-    onSelect: v => {
-      console.log('[onSelect] select result: ', v);
     },
   });
 
