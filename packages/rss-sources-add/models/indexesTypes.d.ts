9386ab7c11c264989b7f0ffc201266523beb4783
zhou-yg <zhou.y.g.890@gmail.com>
2024-07-27 19:46:20 +0800

    feat: test scripts

---
+++ packages/rss-sources-add/models/indexesTypes.d.ts
@@ -0,0 +1,4 @@
+export {
+  RSS,
+  Directory 
+} from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/rss-sources-add/models/customPrismaClient/client/index'
