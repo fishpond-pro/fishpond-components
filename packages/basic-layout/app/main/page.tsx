645aa4330995ab934a189a3e7a14f2d02c83c99e
zhou-yg <zhou.y.g.890@gmail.com>
2024-08-03 23:33:10 +0800

    feat(bl): refactor moduleOverride

--- packages/basic-layout/app/main/page.tsx
+++ packages/basic-layout/app/main/page.tsx
@@ -1,9 +1,11 @@
 'use client'
-import App from '@/app/polymita/views/App'
+import AppFn from '@/app/polymita/views/App'
 import '@/app/polymita/views/AsideNewForTest'
-import { modulesLinkMap } from '@/common/moduleOverride';
+import * as mo from '../moduleOverride';
 
-console.log('modulesLinkMap: ', modulesLinkMap);
+console.log('modulesLinkMap: ', mo.modulesLinkMap);
+
+const App = AppFn(mo.modulesLinkMap, mo.modulesActiveMap)
 
 export default () => {
   return (
