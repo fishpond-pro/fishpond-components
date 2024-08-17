cdb1501a77cbe4a507c5b54824314dee39470308
zhou-yg <zhou.y.g.890@gmail.com>
2024-08-17 15:08:05 +0800

    feat(bl): use new server framework

--- packages/basic-layout/app/layout.tsx
+++ packages/basic-layout/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import './globals.css'
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "fishpond@EA",
@@ -15,11 +12,12 @@ export default function RootLayout({
   children: React.ReactElement;
 }>) {
 
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  );
+  return children
+  // return (
+  //   <html lang="en">
+  //     <body>
+  //       {children}
+  //     </body>
+  //   </html>
+  // );
 }
