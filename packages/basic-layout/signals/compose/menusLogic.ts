5975911d2e2ffaaf4e280abe051bd0d756b6ef3b
zhou-yg <zhou.y.g.890@gmail.com>
2024-09-24 11:24:58 +0800

    feat: test upload zip

---
+++ packages/basic-layout/signals/compose/menusLogic.ts
@@ -0,0 +1,9 @@
+
+//
+// auto generated file
+// provide by
+// warning: auto generated . Do not modify this file
+//
+import d from 'rss-sources/dist/signals/menusLogic'
+Object.assign(d, { __polymita_compose__: true })
+export { default } from 'rss-sources/dist/signals/menusLogic'
---
+++ packages/basic-layout/signals/compose/rss.ts
@@ -0,0 +1,9 @@
+
+//
+// auto generated file
+// provide by
+// warning: auto generated . Do not modify this file
+//
+import d from 'rss-sources/dist/signals/rss'
+Object.assign(d, { __polymita_compose__: true })
+export { default } from 'rss-sources/dist/signals/rss'
