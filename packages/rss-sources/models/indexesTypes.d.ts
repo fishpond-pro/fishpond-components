2d8cab16dd4903701677ab5c5482a603e0f2d8ee
zhou-yg <zhou.y.g.890@gmail.com>
2024-12-26 01:31:09 +0800

    feat(rsq2): using third_part

---
+++ packages/rss-sources/models/indexesTypes.d.ts
@@ -0,0 +1,8 @@
+export {
+  Polymita_message_list_ChannelRecord,
+  Polymita_message_list_Message,
+  Polymita_message_list_MessageContent,
+  Polymita_rss_sources_add_RSS,
+  Polymita_rss_sources_add_Directory,
+  @polymita/rss-sources 
+} from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/rss-sources/models/customPrismaClient/client/index'
