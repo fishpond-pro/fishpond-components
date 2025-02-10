bd89b712b9afa29783f836fb0ca701b31251f45c
zhou-yg <zhou.y.g.890@gmail.com>
2025-02-11 01:09:37 +0800

    feat(rsa): fix types

--- packages/rss-sources-add/models/indexesTypes.d.ts
+++ packages/rss-sources-add/models/indexesTypes.d.ts
@@ -1,5 +1,9 @@
 export {
   RSS,
   Directory,
-  @polymita/rss-sources-add 
+  Polymita_rss_sources_Polymita_message_list_ChannelRecord,
+  Polymita_rss_sources_Polymita_message_list_Message,
+  Polymita_rss_sources_Polymita_message_list_MessageContent,
+  Polymita_rss_sources_Polymita_rss_sources_add_RSS,
+  Polymita_rss_sources_Polymita_rss_sources_add_Directory 
 } from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/rss-sources-add/models/customPrismaClient/client/index'
