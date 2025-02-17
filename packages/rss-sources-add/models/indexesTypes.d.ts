6d2fcd92a66899e15381c7da738f594cab22bf03
zhou-yg <zhou.y.g.890@gmail.com>
2025-02-18 01:12:48 +0800

    feat(rsa): update

--- packages/rss-sources-add/models/indexesTypes.d.ts
+++ packages/rss-sources-add/models/indexesTypes.d.ts
@@ -3,5 +3,6 @@ export {
   Directory,
   Directory44,
   Directory5,
-  Directory6 
+  Directory6,
+  RSS 
 } from '/Users/zhouyunge/Documents/fishpond-desktop-workspace/packages/fishpond-components/packages/rss-sources-add/models/customPrismaClient/client/index'
